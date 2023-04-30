3b137d0b63adfa27820fec86e625ef69c5e3a806
deepakgupta1612 <dg9718221985@gmail.com>
2023-04-30 12:56:22 +0530

    first commit

--- database/config/mongodb.js
+++ database/config/mongodb.js
@@ -1,11 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
-const url =  'mongodb://127.0.0.1:27017'
+  // const url = "mongodb+srv://anjugupta9213:Dg161299%40@cluster0.hk2xety.mongodb.net/?retryWrites=true&w=majority"; 
 
 
 async function getEmpData() {
-    let client = await MongoClient.connect(url);
-    let connection = client.db('issueTracker'); // Establish connection
-    return connection.collection('IssueTracker2023'); // creating a collection and naming it also
+    let client = await MongoClient.connect(process.env.DB_URL);
+     let connection = client.db('issueTracker');   // Establish connection
+   return connection.collection('IssueTracker2023');  // creating a collection and naming it also
 }
 
 module.exports = getEmpData;
+
